1958929a6c5dbc49d584c2276e27bb6ac30dd600
Organic-Punch <sinho667@naver.com>
2020-08-09 16:55:30 +0900

    알바천국구현

---
+++ router/main.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { handleHome, handleLogin } from "../controller/homeController";
+import routes from "../routes";
+
+const homeRouter = express.Router();
+
+homeRouter.get(routes.home, handleHome);
+homeRouter.get(routes.login, handleLogin);
+
+export default homeRouter;

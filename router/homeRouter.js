8d6a302f56bd22e54681a884aa4b5c8775a15169
Organic-Punch <sinho667@naver.com>
2020-08-10 22:56:02 +0900

    알바천국구현

--- router/homeRouter.js
+++ router/homeRouter.js
@@ -7,7 +7,17 @@ const homeRouter = express.Router();
 homeRouter.get(routes.home, handleHome);
 homeRouter.get(routes.login, handleLogin);
 
-homeRouter.post(routes.Main, handleMain);
-homeRouter.get(routes.Main_id, handleMain_id);
+homeRouter.post('/main', function(req, res) {
+    res.send(JSON.stringify("This is main"));
+});
+
+homeRouter.get('/main/:id', function(req, res) {
+    res.send(JSON.stringify("usertype is " + req.param.id));
+});
+
+homeRouter.post('/user/employer', function(req, res) {
+    res.send(JSON.stringify("고용자 데이터 전송" + req.params.id + ' ' + req.params.pw + ' ' + req.params.id));
+})
+
 
 export default homeRouter;

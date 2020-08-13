23e901dd0529fa43dea8d2edc6a506e1ab84c151
Organic-Punch <sinho667@naver.com>
2020-08-13 19:19:51 +0900

    아이디중복및번호입력받기

--- controller/homecontroller.js
+++ controller/homecontroller.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import { models } from "mongoose";
 
 export const handleHome = (req, res) => {
     res.send("HOME!");
@@ -9,26 +10,47 @@ export const handleLogin = (req, res) => {
 };
 
 export const handleJoin = async(req, res) => {
-    res.send("Join");
+    // const userId = req.body.id;
+    // const userPw = req.body.pw;
+    // const userphonenumber = req.body.ph;
+
+    // const newUser = await User.create({
+    //     id: userId,
+    //     pw: userPw,
+    //     phonenumber: userphonenumber,
+    // });
     const userId = req.body.id;
     const userPw = req.body.pw;
-    const userPH = req.body.ph;
+    const userPh = req.body.ph;
+    console.log(req.body.id);
+    console.log(req.body.pw);
+    console.log(req.body.ph);
 
-    const newUser = await User.create({
-        id: userId,
-        pw: userPw,
-        ph: userPH,
-    });
+    var b = true;
+
+    const dbList = await User.find(); //DB 정보를 리스트를 받음
+    for (let i = 0; i < dbList.length; ++i) {
+        if (dbList[i]["id"] == userId) {
+            res.send("중복!");
+            b = false;
+        }
+    }
+    if (b == true) {
+        var newUser = {
+            id: userId,
+            pw: userPw,
+            ph: userPh
+        }
+        const users = await User.create(newUser);
+        res.send("Join" + req.body.id + req.body.pw + req.body.ph);
+    }
+
+
 };
 
 export const handlefind = async(req, res) => {
     res.send("Join");
     const userId = req.body.id;
 
-    const dbList = await User.find(); //DB 정보를 리스트를 받음
-    for (let i = 0; i < dbList.length; ++i) {
-        if (dbList[i]["id"] == userId) {
-            res.send("중복!");
-        }
-    }
+
 };
